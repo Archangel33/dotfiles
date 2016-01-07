d7dfe095232d98758f45d5e1272e6f4f99583493
Michael Reed <Archangel33@users.noreply.github.com>
2016-01-07 11:26:12 -0700

    updated files to allow the install script to work properly

--- .make.sh
+++ .make.sh
@@ -4,40 +4,38 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ###########
 
+###### Helper func
+function timestamp(){
+     date +"%Y%m%d.%H%M%S"
+}
+
 ###### Variables
 
 dir=~/dotfiles                         # dotfiles directory
-olddir=~/dotfiles_old . timestamp()    # old dotfiles backup directory
+olddir=~/dotfiles_old/$(timestamp)     # old dotfiles backup directory
 # list of files/folders to symlink in homedir
 files="vimrc gitconfig gitignore"
 
 ###### 
 
-timestamp(){
-     date +"%Y%m%d.%H%M%S"
-}
+
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~/"
-if ! mkdir -p $olddir 2>/dev/null; then
-    echo "Failed to create $olddir" 
-    exit
-fi
+mkdir -p $olddir
+
 echo "done..."
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
-if ! $PWD == $dir; then
-    echo "Failed to change directory to $dir"
-    exit
-fi
+
 echo "done..."
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving $file from ~/ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
